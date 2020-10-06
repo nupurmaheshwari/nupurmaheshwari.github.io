255d8c348ad9dec9245db2d57ea7d883224e7bdc
nupurmaheshwari <maheshwari.nupur33@gmail.com>
2020-10-06 10:00:45 -0700

    Updates

---
+++ precache-manifest.2ce4f26dcb1f2004177c058e5a273a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71fdaa7acd43c596f2ed4507ea17c072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ca9e560771bf420010d",
+    "url": "/static/css/main.af0e0486.chunk.css"
+  },
+  {
+    "revision": "8a256da35e83d170d4fa",
+    "url": "/static/js/2.093b5cad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.093b5cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca9e560771bf420010d",
+    "url": "/static/js/main.ae3e922c.chunk.js"
+  },
+  {
+    "revision": "621832cc131b6a3350ab",
+    "url": "/static/js/runtime-main.52fdb729.js"
+  }
+]);
