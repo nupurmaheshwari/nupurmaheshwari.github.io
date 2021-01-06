1832babae274eeca99acf4c17e63bb0f659b832d
Nupur Maheshwari <nupurm@andrew.cmu.edu>
2021-01-05 21:24:42 -0800

    homepage : .

---
+++ build/precache-manifest.f2ac46e5c5b4f6e4039edf760634ff1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fa24c74bb86e5ceef60edfe53f509b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfa7f5c76f70a2a5d965",
+    "url": "./static/css/main.9d184b9d.chunk.css"
+  },
+  {
+    "revision": "59ddca4fdbb2c76dd4f6",
+    "url": "./static/js/2.10138f24.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.10138f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa7f5c76f70a2a5d965",
+    "url": "./static/js/main.8ff65bf2.chunk.js"
+  },
+  {
+    "revision": "58a1c326799e4e49aaa6",
+    "url": "./static/js/runtime-main.f623ecad.js"
+  }
+]);
